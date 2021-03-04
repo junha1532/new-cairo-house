06eaca3a33b5945804c69447283fb67be560baec
Junha Park <junha.park@mail.mcgill.ca>
2021-03-04 01:29:21 -0500

    Created Language Selector

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
+const languageSelector = document.querySelector('.language__selector');
+const navMenu = document.querySelector('.navbar__menu');
+const languageMenu = document.querySelector('.language__menu');
+// const icons = document.querySelector('.navbar__icons');
 
 toggleBtn.addEventListener('click', () =>{
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    // icons.classList.toggle('active');
+});
+
+languageSelector.addEventListener('click', () =>{
+    languageSelector.classList.toggle('pressed');
+    languageMenu.classList.toggle('pressed');
 });
